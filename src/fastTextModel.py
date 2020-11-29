bb639b8186f5aa3e46b150d01ad5f151473361da
David Ciria <david.ciria01@estudiant.upf.edu>
2020-11-29 21:58:14 +0100

    Basic workflow finished

---
+++ src/fastTextModel.py
@@ -0,0 +1,58 @@
+import fasttext
+import pandas as pd
+import numpy as np
+import re
+import emoji
+import tweepy
+
+class FastTextModel:
+
+    def __init__(self, screen_user, api):
+        self.screen_user = screen_user
+        self.api = api
+        #Carregar el model preentrenat de fasttext.
+        self.load_model('./data/fastTextModel.model')
+
+    def load_model(self, path):
+        self.model = fasttext.load_model(path)
+
+    #Function to clean the text of a tweet.
+    def clean_tweet(self, text):
+        #clean_text = re.sub("@.*:", "", text) #This do not improve performance.
+        clean_text = re.sub("@.*:", "", text) #This do not improve performance.
+        clean_text = re.sub("RT", "", clean_text)
+        clean_text = re.sub("\n", " ", clean_text)
+        #Separate emojis from words.
+        """
+        sep_emojis_text = ""
+        for c in clean_text:
+            if c in emoji.UNICODE_EMOJI:
+                sep_emojis_text+=' '+c+' '
+            else:
+                sep_emojis_text+=c
+        #This do not improve performance.
+        """
+        return clean_text
+
+    def getScores(self):
+        partiesCountDict = {}
+        timeline = self.api.user_timeline(screen_name = self.screen_user, count = 200, include_rts = True,tweet_mode='extended')
+        for tweet in timeline:
+            parties, scores = self.model.predict(self.clean_tweet(tweet.full_text), k=5)
+            print(scores, parties)
+            #if scores[0] > 0.7:
+            #    if parties[0] in partiesCountDict:
+            #        partiesCountDict[parties[0]] += 1
+            #    else:
+            #        partiesCountDict[parties[0]] = 1
+            for p,s in zip(parties, scores):
+                if s > 0.6:
+                    if p in partiesCountDict:
+                        partiesCountDict[p] += s
+                    else:
+                        partiesCountDict[p] = s
+        
+        sum_scores = np.sum([*partiesCountDict.values()])
+
+        for k, v in partiesCountDict.items():
+            print(k, v/sum_scores)
